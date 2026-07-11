6aa4bdbb489f08784490365cbeff6530bb5ebccb
ariaputrab <114902342+ariaputrab@users.noreply.github.com>
2026-07-11 16:50:37 +0700

    Add files via upload

---
+++ pengaturan.js
@@ -0,0 +1,16 @@
+// =========================================================================
+// PENGATURAN APLIKASI CLIENT PORTAL
+// Silakan ubah teks di dalam tanda kutip ("...") dengan data studio Anda.
+// =========================================================================
+
+const CONFIG_APP = {
+    // 1. Nama Studio Foto Anda (Akan tampil di header dan portal klien)
+    namaVendor: "VIVIDLENS GALLERY",
+
+    // 2. Google Drive API Key Baru Milik Anda (Penting agar galeri bisa memuat foto)
+    googleApiKey: "AIzaSyB-2Gn_Hm3xyyOK3LYw4qDsefTLctn6OQM",
+
+    // 3. Konfigurasi Database Supabase Anda
+    supabaseUrl: "https://aizhweokxpxfrczqsuik.supabase.co",
+    supabaseKey: "sb_publishable_bEKjQ-KQvs3GZpH0eVaHbw_N0ssNYKp"
+};Í
